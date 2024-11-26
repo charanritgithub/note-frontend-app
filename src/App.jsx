b5cd7f9e4ee6a3bf08e75c1f6d3a856d0d7b72c6
charanritgithub <165257074+charanritgithub@users.noreply.github.com>
2024-11-26 21:41:05 +0530

    changed signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Signup from './pages/Signup'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Signup from './pages/Signup'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
